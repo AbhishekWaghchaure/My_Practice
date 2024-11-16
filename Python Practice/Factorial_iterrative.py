87fe7e3298fe466df58de3ab3b916608cba9ebb6
AbhishekWaghchaure <abhisw28@gmail.com>
2024-11-16 20:31:46 +0530

    did basics of python in depth

--- Python Practice/Factorial_iterrative.py
+++ Python Practice/Factorial_iterrative.py
@@ -3,7 +3,7 @@ num = int(input("Enter the value of number whose factorial we want to count"))
 
 if num < 0:
     print("Factorial does not exist for negative numbers")
-elif (num == 0):
+elif (num == 0): 
     print("Factorial of 0 is 1")
 else:
     for i in range(1, num + 1):
